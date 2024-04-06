ad010f3a8246687ff6889d7e093aa6ec21fc682a
Hamama-Komal <hamama.komal.00@gmail.com>
2024-04-06 16:02:56 +0500

    Context Switching

--- app/src/main/java/com/example/channelsflows/MainActivity.kt
+++ app/src/main/java/com/example/channelsflows/MainActivity.kt
@@ -7,17 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
-import kotlin.system.measureTimeMillis
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,55 +31,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // Flow Events
+        // Context Switching
 
-        //  onStart, onEach, onCompletion
-        // Consumer 1
-        GlobalScope.launch {
+        //  flowOn()
+        // Consumer
+        GlobalScope.launch(Dispatchers.Main) {  // consumer => Main Thread
             producer()
-                .onStart {
-                    emit(0)
-                    Log.d("FLOW_RESULT_1", "Stream Start")
-
-                }
-                .onEach {
-                    Log.d("FLOW_RESULT_1", "About to emit $it")
-                }
-                .onCompletion {
-                    emit(6)
-                    Log.d("FLOW_RESULT_1", "Stream Ends")
-                }
-                .collect {
-                    Log.d("FLOW_RESULT_1", it.toString())
-                }
-        }
-
-        // buffer
-        // Consumer 2
-        GlobalScope.launch {
-
-            val time = measureTimeMillis {
-                producer().buffer(2).collect {
-                    delay(2000)
-                    Log.d("FLOW_RESULT_2", it.toString())
-                }
-            }
-            Log.d("FLOW_RESULT_2", "Total time consume $time")
-        }
-
-
-        // map & filter
-        //  Consumer 3
-        GlobalScope.launch {
-            producer()
+                .flowOn(Dispatchers.Main)      // producer => Main Thread
                 .map {
+                    delay(2000)
+                    Log.d("FLOW_RESULT", "Map Thread - ${Thread.currentThread().name}")
                     it * 2 + 2
                 }
                 .filter {
+                    delay(1000)
+                    Log.d("FLOW_RESULT", "Filter Thread - ${Thread.currentThread().name}")
                     it <= 10
                 }
+                .flowOn(Dispatchers.IO)               // filter & map => IO Thread
                 .collect {
-                    Log.d("FLOW_RESULT_3", it.toString())
+                    Log.d("FLOW_RESULT", "Collector Thread - ${Thread.currentThread().name}")
                 }
         }
 
@@ -94,6 +63,7 @@ class MainActivity : AppCompatActivity() {
         val list = listOf(1, 2, 3, 4, 5)
         list.forEach {
             delay(1000)
+            Log.d("FLOW_RESULT", "Emitter Thread - ${Thread.currentThread().name}")
             emit(it)
         }
 
